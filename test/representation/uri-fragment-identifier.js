72d4861d5926c9c457319b766c9e801679c7264e
Vladimir Gorej <vladimir.gorej@gmail.com>
2025-02-22 22:51:27 +0100

    test(representation): include URI Fragment example from RFC

--- test/representation/uri-fragment-identifier.js
+++ test/representation/uri-fragment-identifier.js
@@ -18,6 +18,21 @@ describe('URI Fragment Identifier (to/from)', function () {
     specify('should return "#" for an empty JSON Pointer', function () {
       assert.strictEqual(URIFragmentIdentifier.to(''), '#');
     });
+
+    specify('should correctly convert JSON Pointer to URI Fragment (RFC 6901)', function () {
+      assert.strictEqual(URIFragmentIdentifier.to(''), '#');
+      assert.strictEqual(URIFragmentIdentifier.to('/foo'), '#/foo');
+      assert.strictEqual(URIFragmentIdentifier.to('/foo/0'), '#/foo/0');
+      assert.strictEqual(URIFragmentIdentifier.to('/'), '#/');
+      assert.strictEqual(URIFragmentIdentifier.to('/a~1b'), '#/a~1b');
+      assert.strictEqual(URIFragmentIdentifier.to('/c%d'), '#/c%25d');
+      assert.strictEqual(URIFragmentIdentifier.to('/e^f'), '#/e%5Ef');
+      assert.strictEqual(URIFragmentIdentifier.to('/g|h'), '#/g%7Ch');
+      assert.strictEqual(URIFragmentIdentifier.to('/i\\j'), '#/i%5Cj');
+      assert.strictEqual(URIFragmentIdentifier.to('/k"l'), '#/k%22l');
+      assert.strictEqual(URIFragmentIdentifier.to('/ '), '#/%20');
+      assert.strictEqual(URIFragmentIdentifier.to('/m~0n'), '#/m~0n');
+    });
   });
 
   context('from()', function () {
@@ -44,5 +59,23 @@ describe('URI Fragment Identifier (to/from)', function () {
     specify('should return the original input if decoding fails', function () {
       assert.strictEqual(URIFragmentIdentifier.from('%E3%81%'), '%E3%81%'); // Malformed UTF-8 sequence
     });
+
+    specify(
+      'should correctly convert between JSON Pointer and URI Fragment (RFC 6901)',
+      function () {
+        assert.strictEqual(URIFragmentIdentifier.from('#'), '');
+        assert.strictEqual(URIFragmentIdentifier.from('#/foo'), '/foo');
+        assert.strictEqual(URIFragmentIdentifier.from('#/foo/0'), '/foo/0');
+        assert.strictEqual(URIFragmentIdentifier.from('#/'), '/');
+        assert.strictEqual(URIFragmentIdentifier.from('#/a~1b'), '/a~1b');
+        assert.strictEqual(URIFragmentIdentifier.from('#/c%25d'), '/c%d');
+        assert.strictEqual(URIFragmentIdentifier.from('#/e%5Ef'), '/e^f');
+        assert.strictEqual(URIFragmentIdentifier.from('#/g%7Ch'), '/g|h');
+        assert.strictEqual(URIFragmentIdentifier.from('#/i%5Cj'), '/i\\j');
+        assert.strictEqual(URIFragmentIdentifier.from('#/k%22l'), '/k"l');
+        assert.strictEqual(URIFragmentIdentifier.from('#/%20'), '/ ');
+        assert.strictEqual(URIFragmentIdentifier.from('#/m~0n'), '/m~0n');
+      },
+    );
   });
 });
