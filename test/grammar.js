4242b96c6c3763f9670a0f81572408b12183b5da
Vladimir Gorej <vladimir.gorej@gmail.com>
2025-02-22 22:07:18 +0100

    test(grammar): add first test

--- test/grammar.js
+++ test/grammar.js
@@ -2,7 +2,7 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 import { assert } from 'chai';
 
-import { Grammar } from '../src/index.js'
+import { Grammar } from '../src/index.js';
 
 describe('SABNF', function () {
   it('should export Grammar', function () {
@@ -10,10 +10,10 @@ describe('SABNF', function () {
   });
 
   it('should convert to string', function () {
-    const abnfFilePath = path.join('..', 'src', 'runtime-expression.bnf');
+    const abnfFilePath = path.join('..', 'src', 'grammar.bnf');
     const abnf = fs.readFileSync(new URL(abnfFilePath, import.meta.url)).toString();
     const grammar = new Grammar();
 
     assert.strictEqual(abnf, grammar.toString());
-  })
-})
+  });
+});
