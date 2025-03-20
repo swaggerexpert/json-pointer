381acdd6008d2cd45dd19e1fa630f4f05cdce4de
Vladimír Gorej <vladimir.gorej@gmail.com>
2025-03-20 10:23:39 +0100

    feat(evaluate): add support for ApiDOM realm (#18)
    Refs #13

--- test/evaluate/realms/apidom.js
+++ test/evaluate/realms/apidom.js
@@ -1,4 +1,6 @@
 import { assert } from 'chai';
+import { ObjectElement, MemberElement, toValue } from '@swagger-api/apidom-core';
+import { InfoElement } from '@swagger-api/apidom-ns-openapi-3-0';
 
 import {
   evaluate,
@@ -8,28 +10,28 @@ import {
   JSONPointerEvaluateError,
   URIFragmentIdentifier,
 } from '../../../src/index.js';
-import MapSetEvaluationRealm from '../../../src/evaluate/realms/map-set.js';
+import ApiDOMEvaluationRealm from '../../../src/evaluate/realms/apidom.js';
 
 describe('evaluate', function () {
-  context('Map/Set realm', function () {
-    const realm = new MapSetEvaluationRealm();
-    const data = new Map([
-      ['foo', new Set(['bar', 'baz'])],
-      ['', 0],
-      ['a/b', 1],
-      ['c%d', 2],
-      ['e^f', 3],
-      ['g|h', 4],
-      ['i\\j', 5],
-      ['k"l', 6],
-      [' ', 7],
-      ['m~n', 8],
-    ]);
+  context('ApiDOM realm', function () {
+    const realm = new ApiDOMEvaluationRealm();
+    const element = new ObjectElement({
+      foo: ['bar', 'baz'],
+      '': 0,
+      'a/b': 1,
+      'c%d': 2,
+      'e^f': 3,
+      'g|h': 4,
+      'i\\j': 5,
+      'k"l': 6,
+      ' ': 7,
+      'm~n': 8,
+    });
 
     context('RFC 6901 JSON String tests', function () {
       const jsonStringRepEntries = [
-        ['', data],
-        ['/foo', new Set(['bar', 'baz'])],
+        ['', toValue(element)],
+        ['/foo', ['bar', 'baz']],
         ['/foo/0', 'bar'],
         ['/', 0],
         ['/a~1b', 1],
@@ -44,15 +46,17 @@ describe('evaluate', function () {
 
       jsonStringRepEntries.forEach(([jsonString, expected]) => {
         specify('should correctly evaluate JSON Pointer from JSON String', function () {
-          assert.deepEqual(evaluate(data, jsonString, { realm }), expected);
+          const actual = toValue(evaluate(element, jsonString, { realm }));
+
+          assert.deepEqual(actual, expected);
         });
       });
     });
 
     context('RFC 6901 URI Fragment Identifier tests', function () {
       const fragmentRepEntries = [
-        ['#', data],
-        ['#/foo', new Set(['bar', 'baz'])],
+        ['#', toValue(element)],
+        ['#/foo', ['bar', 'baz']],
         ['#/foo/0', 'bar'],
         ['#/', 0],
         ['#/a~1b', 1],
@@ -67,64 +71,88 @@ describe('evaluate', function () {
 
       fragmentRepEntries.forEach(([fragment, expected]) => {
         specify('should correctly evaluate JSON Pointer from URI Fragment Identifier', function () {
-          assert.deepEqual(
-            evaluate(data, URIFragmentIdentifier.from(fragment), { realm }),
-            expected,
+          const actual = toValue(
+            evaluate(element, URIFragmentIdentifier.from(fragment), {
+              realm,
+            }),
           );
+
+          assert.deepEqual(actual, expected);
         });
+      });
+    });
+
+    context('given ApiDOM namespace element', function () {
+      specify('should evaluate', function () {
+        const infoElement = InfoElement.refract({
+          contact: {
+            name: 'SwaggerExpert',
+            email: 'contact@swaggerexpert.com',
+          },
+        });
+        const actual = toValue(
+          evaluate(infoElement, '/contact/name', {
+            realm: new ApiDOMEvaluationRealm(),
+          }),
+        );
+
+        assert.strictEqual(actual, 'SwaggerExpert');
       });
     });
 
     context('invalid JSON Pointers (should throw errors)', function () {
       specify('should throw JSONPointerEvaluateError for invalid JSON Pointer', function () {
-        assert.throws(() => evaluate(data, 'invalid-pointer', { realm }), JSONPointerEvaluateError);
+        assert.throws(
+          () => evaluate(element, 'invalid-pointer', { realm }),
+          JSONPointerEvaluateError,
+        );
       });
 
       specify(
         'should throw JSONPointerTypeError for accessing property on non-object/array',
         function () {
-          assert.throws(() => evaluate(data, '/foo/0/bad', { realm }), JSONPointerTypeError);
+          assert.throws(() => evaluate(element, '/foo/0/bad', { realm }), JSONPointerTypeError);
         },
       );
 
       specify('should throw JSONPointerKeyError for non-existing key', function () {
-        assert.throws(() => evaluate(data, '/nonexistent', { realm }), JSONPointerKeyError);
+        assert.throws(() => evaluate(element, '/nonexistent', { realm }), JSONPointerKeyError);
       });
 
       specify('should throw JSONPointerIndexError for non-numeric array index', function () {
-        assert.throws(() => evaluate(data, '/foo/x', { realm }), JSONPointerIndexError);
+        assert.throws(() => evaluate(element, '/foo/x', { realm }), JSONPointerIndexError);
       });
 
       specify('should throw JSONPointerIndexError for out-of-bounds array index', function () {
-        assert.throws(() => evaluate(data, '/foo/5', { realm }), JSONPointerIndexError);
+        assert.throws(() => evaluate(element, '/foo/5', { realm }), JSONPointerIndexError);
       });
 
       specify('should throw JSONPointerIndexError for leading zero in array index', function () {
-        assert.throws(() => evaluate(data, '/foo/01', { realm }), JSONPointerIndexError);
+        assert.throws(() => evaluate(element, '/foo/01', { realm }), JSONPointerIndexError);
       });
 
       specify('should throw JSONPointerIndexError for "-" when strictArrays is true', function () {
         assert.throws(
-          () => evaluate(data, '/foo/-', { strictArrays: true, realm }),
+          () => evaluate(element, '/foo/-', { strictArrays: true, realm }),
           JSONPointerIndexError,
         );
       });
 
       specify('should return undefined for "-" when strictArrays is false', function () {
-        assert.strictEqual(evaluate(data, '/foo/-', { strictArrays: false, realm }), undefined);
+        assert.strictEqual(evaluate(element, '/foo/-', { strictArrays: false, realm }), undefined);
       });
 
       specify(
         'should throw JSONPointerKeyError for accessing chain of object properties that do not exist',
         function () {
-          assert.throws(() => evaluate(data, '/missing/key', { realm }), JSONPointerKeyError);
+          assert.throws(() => evaluate(element, '/missing/key', { realm }), JSONPointerKeyError);
         },
       );
 
       specify(
         'should return undefined accessing object property that does not exist when strictObject is false',
         function () {
-          assert.isUndefined(evaluate(data, '/missing', { strictObjects: false, realm }));
+          assert.isUndefined(evaluate(element, '/missing', { strictObjects: false, realm }));
         },
       );
 
@@ -138,6 +166,15 @@ describe('evaluate', function () {
           assert.throws(() => evaluate({ foo: 42 }, '/foo/bar', { realm }), JSONPointerTypeError);
         },
       );
+    });
+
+    context('given member name is not unique in an object', function () {
+      specify('should throw JSONPointerKeyError', function () {
+        const objectElement = new ObjectElement({ a: 'b' });
+        objectElement.content.push(new MemberElement('a', 'c'));
+
+        assert.throws(() => evaluate(objectElement, '/a', { realm }), JSONPointerKeyError);
+      });
     });
   });
 });
